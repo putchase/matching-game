d4cd049ba089c971c735f558b8240bdd67e7cbfd
Fellipe Pereira <fellipepereira@MacBook-Pro-de-Fellipe.local>
2021-07-11 19:29:18 -0300

    create the boardgamee component

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ import './src/styles/settings/colors.css'
 import './src/styles/elements/base.css'
 
 import CardGame from './src/components/CardGame'
+import PlayerScore from './src/components/PlayerScore'
+import BoardGame from './src/objects/BoardGame'
 
 const $root = document.querySelector("#root");
-const $htmlCardGame = CardGame();
+const $playerScore = PlayerScore('Player1');
+const $htmlBoardGame = BoardGame(6);
 
-
-$root.insertAdjacentHTML('beforeend', $htmlCardGame);
+$root.insertAdjacentHTML('beforeend', $playerScore);
+$root.insertAdjacentHTML('beforeend', $htmlBoardGame);
---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+function PlayerName (playerName){
+  return `
+      <h2> ${playerName} </h2>
+    `
+}
+
+export default PlayerName;
